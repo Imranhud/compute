c6401ecd4272d34055940b54cc59b503fd2d6329
Imranhud <142754507+Imranhud@users.noreply.github.com>
2024-10-29 13:48:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import HeadV from './components/HeadV'
-import Hero from './components/Hero'
-import Cliets from './components/Cliets'
-import Card from './components/Card'
-import Footer from './components/Footer copy'
-import GetStated from './components/GetStated'
-import SocialLinks from './components/Sociallinks'
+// import HeadV from './components/HeadV'
+// import Hero from './components/Hero'
+// import Cliets from './components/Cliets'
+// import Card from './components/Card'
+// import Footer from './components/Footer copy'
+// import GetStated from './components/GetStated'
+// import SocialLinks from './components/Sociallinks'
 
 const App = () => {
   return (
-      <div className='bg-black w-full overflow-auto'>
-      <HeadV />
-      <Hero />
-      <Cliets />
-      <Card />
-      <GetStated />
-      <SocialLinks/>
-      <Footer />
+    <div className="">
+  <h1>Hllo old</h1>
+    </div>
+      // <div className='bg-black w-full overflow-auto'>
+      // <HeadV />
+      // <Hero />
+      // <Cliets />
+      // <Card />
+      // <GetStated />
+      // <SocialLinks/>
+      // <Footer />
     </div>
   )
 }
