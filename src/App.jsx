48c8bf04d5415b19dd3f1c602bd8221404f11bbc
Imranhud <142754507+Imranhud@users.noreply.github.com>
2024-10-29 13:57:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import React from 'react'
-// import HeadV from './components/HeadV'
-// import Hero from './components/Hero'
-// import Cliets from './components/Cliets'
-// import Card from './components/Card'
-// import Footer from './components/Footer copy'
-// import GetStated from './components/GetStated'
-// import SocialLinks from './components/Sociallinks'
-
+import Vid from './components/Auth'
 const App = () => {
   return (
-    <div className="">
-  <h1>Hllo old</h1>
+      <div className='bg-black w-full overflow-auto'>
+      <div className="header">
+    <h1>Fatima Class Room </h1>
+  </div>
+      <Vid/>
     </div>
-      // <div className='bg-black w-full overflow-auto'>
-      // <HeadV />
-      // <Hero />
-      // <Cliets />
-      // <Card />
-      // <GetStated />
-      // <SocialLinks/>
-      // <Footer />
-    // </div>
   )
 }
-
-export default App
+  
+export default App;
